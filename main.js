fea0e431879f26deb8f63e6451a26b4476422bc6
Yi-Cong, Kuo <c4493124@gmail.com>
2017-10-02 00:12:40 +0800

    取得時間查詢字串

--- main.js
+++ main.js
@@ -6,8 +6,6 @@ require([
 	"dojo/dom",
 	"dijit/registry",
 
-	"dojo/date/stamp",
-
 	"dijit/layout/BorderContainer",
 	"dijit/layout/ContentPane",
 	"dijit/layout/AccordionContainer",
@@ -19,8 +17,7 @@ require([
 	"dojo/domReady!"
 ], function (
 	Map, MapView,
-	parser, dom, registry,
-	stamp
+	parser, dom, registry
 ){
 
 	// 解析dojo元素
@@ -41,13 +38,31 @@ require([
 	//執行查詢功能
 	var queryTimeWidget = registry.byId('queryTime');
 		queryTimeWidget.set('onClick', function (){
-			var startDateWidget = registry.byId('startDate'),
-				startTimeWidget = registry.byId('startTime');
+			// 取得起訖日期與時間
+			var startDate = registry.byId('startDate').get('value'),
+				startTime = registry.byId('startTime').get('value'),
+				endDate   = registry.byId('endDate').get('value'),
+				endTime   = registry.byId('endTime').get('value');
 
-			console.log(startTimeWidget.get('value'));
+			if(!startDate){ alert("請選擇起始日期!"); return;}
+			if(!startTime){ alert("請選擇起始時間!"); return;}
+			if(!endDate){ alert("請選擇終止日期!"); return;}
+			if(!endTime){ alert("請選擇終止時間!"); return;}
 
-			var dateString = stamp.toISOString(startDateWidget.get('value'), {selector: 'date'});
-			console.log(dateString);
+			if(startDate > endDate){ alert("起始日大於終止日，無法運算!"); return;}
+			
+			if( startDate.valueOf() === endDate.valueOf() && 
+				endTime.valueOf() < startTime.valueOf()
+			){ 
+				alert("起始時間大於終止時間，無法運算!"); 
+				return;	
+			}
+
+			// 將日期時間轉換成查詢字串
+			var startStr = transferDateTime(startDate, startTime),
+				endStr = transferDateTime(endDate, endTime);
+
+			console.log(startStr, endStr); 
 		});
 
 
@@ -55,19 +70,41 @@ require([
 
 	}
 
-	function addGraphics(graphics){
+	function addToMap(graphics){
 
 	}
 
-	function removeGraphics(graphics){
+	function rmFromMap(graphics){
 
 	}
 
-	function addTable(graphics){
+	function addToTable(graphics){
 
 	}
 
-	function removeTable(){
+	function rmFromTable(graphics){
 
+	}
+
+	function transferDateTime(date, time) {
+		var result  = '',
+			year    = date.getFullYear(),
+			month   = date.getMonth() +　1,
+			day     = date.getDate(),
+			hours   = time.getHours(),
+			minutes = time.getMinutes(),
+			sec     = time.getSeconds();
+
+			year    = String(year);
+		    month   = (month < 10)  ?('0' + String(month))   : String(month);
+		    day     = (day < 10)    ?('0' + String(day))     : String(day);
+		    hours   = (hours < 10)  ?('0' + String(hours))   : String(hours);
+		    minutes = (minutes < 10)?('0' + String(minutes)) : String(minutes);
+		    sec     = (sec < 10)    ?('0' + String(sec))     : String(sec);
+
+		    result = year + '/' + month + '/' + day + ' ' 
+		    	   + hours + ':' + minutes + ':' + sec;
+
+		return result; 
 	}
 });
